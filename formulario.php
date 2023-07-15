769092eb4698b2e06fae2d2177318b67c2e23a76
LuizBlume <luiz.fernando.251107@gmail.com>
2023-07-15 02:02:30 -0300

    finalmente cadastro funcionando

--- formulario.php
+++ formulario.php
@@ -21,14 +21,15 @@ if (isset($_POST['submit'])) {
 
   $nome = $_POST['nome'];
   $email = $_POST['email'];
+  $senha = $_POST['senha'];
   $telefone = $_POST['telefone'];
   $sexo = $_POST['genero'];
   $data_nasc = $_POST['data_nascimento'];
   $cidade = $_POST['cidade'];
   $estado = $_POST['estado'];
   $endereco = $_POST['endereco'];
-
-  $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, telefone, sexo, data_nasc, cidade, estado, endereco) VALUES ('$nome', '$email', '$telefone', '$sexo', '$data_nasc', '$cidade', '$estado', '$endereco')");
+  
+  $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, senha, telefone, sexo, data_nasc, cidade, estado, endereco) VALUES ('$nome', '$email', '$senha', '$telefone', '$sexo', '$data_nasc', '$cidade', '$estado', '$endereco')");
 }
 
 ?>
