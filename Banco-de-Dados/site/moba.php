5fcc3a3aa4765652e44078b301e7c8cd6883e3fe
LuizBlume <luiz.fernando.251107@gmail.com>
2023-07-21 21:15:14 -0300

    footer finalizado, falta responsividade

--- Banco-de-Dados/site/moba.php
+++ Banco-de-Dados/site/moba.php
@@ -6,11 +6,161 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="shortcut icon" href="imagens/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+		integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>MOBA</title>
     <style>
         .anuncio-meio {
             width: 870px;
             height: 120px;
+        }
+
+        main {
+            height: 2320px;
+        }
+
+        * {
+            letter-spacing: 1px;
+        }
+
+        header {
+            box-shadow: 0 5px 0 #171717;
+        }
+
+        footer {
+            width: 100%;
+            color: #ffffff;
+        }
+
+        #footer-link {
+            text-decoration: none;
+        }
+
+        #footer_content {
+            background-color: #202020;
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            padding: 3rem 3.5rem;
+        }
+
+        #footer_contacts h1 {
+            margin-bottom: 0.75rem;
+        }
+
+        #footer_social_media {
+            display: flex;
+            gap: 2rem;
+            margin-top: 1.5rem;
+        }
+
+        #footer_social_media .footer-link {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 2.5rem;
+            width: 2.5rem;
+            color: var(--color-neutral-30);
+            border-radius: 50%;
+            transition: all 0.4s;
+        }
+
+        #footer_social_media .footer-link:hover {
+            opacity: 0.8;
+        }
+
+        #instagram {
+            background: linear-gradient(#7f37c9, #ff2992, #ff9807);
+        }
+
+        #facebook {
+            background-color: #4267b3;
+        }
+
+        #whatsapp {
+            background-color: #25d366;
+        }
+
+        .footer-list {
+            display: flex;
+            flex-direction: column;
+            gap: 0.75rem;
+            list-style: none;
+        }
+
+        .footer-list .footer-link {
+            color: var(--color-neutral-30);
+            transition: all 0.4s;
+        }
+
+        .footer-list .footer-link:hover {
+            color: var(---color-neutral-30);
+        }
+
+        #footer_subscribe {
+            display: flex;
+            flex-direction: column;
+            gap: 1.5rem;
+        }
+
+        #footer_subscribe p {
+            color: var(---color-neutral-20);
+        }
+
+        #input_group {
+            display: flex;
+            align-items: center;
+            background-color: var(---color-neutral-0);
+            border-radius: 4px;
+        }
+
+        #input_group input {
+            all: unset;
+            padding: 0.75rem;
+            width: 100%;
+        }
+
+        #input_group button {
+            background-color: var(---color-neutral-20);
+            border: none;
+            color: var(---color-neutral-20);
+            padding: 0px 1.25rem;
+            font-size: 1.125rem;
+            height: 100%;
+            border-radius: 0px 4px 4px 0px;
+            cursor: pointer;
+            transition: all 0.4s;
+        }
+
+        #input_group button:hover {
+            opacity: 0.8;
+        }
+
+        #footer_copyright {
+            display: flex;
+            justify-content: center;
+            background-color: var(---color-neutral-0);
+            font-size: 0.9rem;
+            padding: 1.5rem;
+            font-weight: 100;
+        }
+
+        @media screen and (max-width: 768px) {
+            #footer_content {
+                grid-template-columns: repeat(2, 1fr);
+                gap: 2rem;
+            }
+        }
+
+        @media screen and (max-width: 426px) {
+            #footer_content {
+                grid-template-columns: repeat(1, 1fr);
+                padding: 3rem 2rem;
+            }
+        }
+
+        #logofooter {
+            border-radius: 180px;
         }
     </style>
 </head>
@@ -115,8 +265,76 @@
             </a>
         </nav>
     </aside>
-    <footer>Jogue um jogo feito por mim!
-        <a href="jogo/jogo.php">Clique aqui</a>
+    <footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <a href="index.php">
+                    <img src="imagens/logo.png" alt="" id="logofooter">
+                </a>
+                <p>it's all about your dreams.</p>
+                <div id="footer_social_media">
+                    <a href="https://www.instagram.com/blume.047/" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=100025262752685" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="https://web.whatsapp.com/" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </div>
+            </div>
+            <ul class="footer-list">
+                <li>
+                    <h3>
+                        Blog
+                    </h3>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Tech</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Adventures</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Music</a>
+                </li>
+            </ul>
+            <ul class="footer-list">
+                <li>
+                    <h3>
+                        Products
+                    </h3>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">app</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Desktop</a>
+                </li>
+                <li>
+                    <a href="#" class="footer-link">Cloud</a>
+                </li>
+            </ul>
+            <div id="footer_subscribe">
+                <h3>Subscribe</h3>
+                <p>
+                    Entre com seu email para notificarmos novas notícias.
+                </p>
+
+                <div id="input_group">
+                    <input type="email" id="email">
+                    <button>
+                        <i class="fa-regular fa-envelope"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169
+            2023 all rights reseverds.
+        </div>
     </footer>
     <script type="text/javascript" src="javascript.js"></script>
 </body>
